0234cc31484db07d7826ad4cd2bea3e9b5c27bbb
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-24 23:16:32 +0100

    Update and rename 1.sh to SLURM.sh

---
+++ SLURM.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+clear
+echo "ESTE ES SU NOMBRE DE EQUIPO ACTUAL:"
+hostname
+echo "¿QUIERE CAMBIARLO?"
+echo "1. Si"
+echo "2. No"
+echo -n "Seleccione una opcion [1 - 2]"
+  read seleccion
+  case $seleccion in
+     1)
+        echo "Escriba el nuevo nombre del equipo:"
+        read nuevohostname
+        hostname
+     ;;
+     2)
+        echo "No cambiado, se mantiene:"
+        hostname
+     ;;
+     *)
+        echo "Numero no reconocido."
+     ;;
