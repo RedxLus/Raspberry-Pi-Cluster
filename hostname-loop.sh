eb9f6333995de079c00d8737ea0fa034d22a87ca
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-29 17:31:15 +0100

    Update hostname-loop.sh

--- hostname-loop.sh
+++ hostname-loop.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-local=/etc/hosts
+archivohosts=/etc/hosts
+echo "¿Cuantas Ips y Hostnames quieres añadir?"
+echo "Escribe el numero por ejemplo 3"
+echo""
+read FINAL
 
-for x in 1 2 3 4 5 6 7 8 9 10
+for x in $(seq 1 $FINAL)
 do
 echo "Inserte la IP$x:"
 read ip0$x
 echo "Inserte el hostname$x:"
 read hostname0$x
-echo "$ip0$x      $hostname0$x" >> $local
+echo "$ip0$x      $hostname0$x" >> $archivohosts
 sleep 2
 done
+
+cat $archivohosts
