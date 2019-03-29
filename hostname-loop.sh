f295b479d9f013a6a1639440118ed53f6ef3b935
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-29 17:09:10 +0100

    Update hostname-loop.sh

--- hostname-loop.sh
+++ hostname-loop.sh
@@ -4,6 +4,10 @@ local=/etc/hosts
 
 for x in 1 2 3 4 5 6 7 8 9 10
 do
-curl "$base_url/$x" > "$fname-$x"
+echo "Inserte la IP$x:"
+read ip0$x
+echo "Inserte el hostname$x:"
+read hostname0$x
+echo "$ip0$x      $hostname0$x" >> $local
 sleep 2
 done
