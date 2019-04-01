a8f3a491587e8a623b64bb1677769e6f38d7f9ed
RedxLus <luisdieguezbarrio@gmail.com>
2019-04-01 15:40:47 +0200

    Update SLURM.sh

--- SLURM.sh
+++ SLURM.sh
@@ -8,8 +8,8 @@ sh Nuevo-hostname-propio.sh
 echo "a. Añadir muchas IPs y Hostnames" 
 echo "b. Añadir solo 1" 
 echo -n "Seleccione una opcion [a - b]"
-  read ipyhostname
-  case $ipyhostname in
+  read ip_y_hostname
+  case $ip_y_hostname in
      a|A)
         wget https://raw.githubusercontent.com/RedxLus/Raspberry-Pi-Cluster/master/Archivos/IP-hostname-loop.sh --no-check-certificate
         chmod +x IP-hostname-loop.sh
@@ -33,9 +33,9 @@ mv slurm.conf.simple slurm.conf
 
 
 apt-get install net-tools
-laip=$(ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
+la_ip=$(ifconfig|awk 'NR == 2'|awk '{print $2}'|cut -d ':' -f2)
 
-echo "Su IP es $laip. ¿Es correcta?. Puede modificarla despues en /etc/slurm-llnl/slurm.conf "
+echo "Su IP es $la_ip. ¿Es correcta?. Puede modificarla despues en /etc/slurm-llnl/slurm.conf "
 echo "1. Si. Quiero añadirla automaticamente para que funcione el Cluster (la IP se añadira al archivo de configuracion)."
 echo "2. No. No es mi ip o quiero hacerlo manualmente."
 echo -n "Seleccione una opcion [1 - 2]"
@@ -45,7 +45,7 @@ if [ $respuesta = 1 ]
 then
 sed -i "s/ControlMachine=laptop/ControlMachine=$HOSTNAME/g" /etc/slurm-llnl/slurm.conf
 sed -i "s/#ControlAddr=/ControlAddr=/g" /etc/slurm-llnl/slurm.conf
-sed -i "s/ControlAddr=/ControlAddr=$laip/g" /etc/slurm-llnl/slurm.conf
+sed -i "s/ControlAddr=/ControlAddr=$la_ip/g" /etc/slurm-llnl/slurm.conf
 else
 echo "Saliendo"
 fi
