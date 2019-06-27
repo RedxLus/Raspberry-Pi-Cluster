9722385a333bebd060825b74388a30c05f280d23
RedxLus <luisdieguezbarrio@gmail.com>
2019-06-27 11:42:49 +0200

    Update Kubernetes-Hypriot.sh

--- Virtual/Kubernetes-Hypriot.sh
+++ Virtual/Kubernetes-Hypriot.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-sudo su -
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
